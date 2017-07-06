63c1c16d1897154bcc88bb13d1d960082d64d519
YuhaoCheng <446358161@qq.com>
2017-07-06 18:55:19 +0800

    Source Code
    Without thread

---
+++ SouceCode_1.0/relay/Query.java
@@ -0,0 +1,42 @@
+package relay;
+import java.net.*;
+public class Query {
+	private DatagramPacket packet;
+    public String domainName;
+    public int queryType;
+    public int queryClass;
+    public int queryStop;
+    
+	public Query(DatagramPacket p){
+		this.packet = p;
+		byte[] data = packet.getData();
+        String name = "";
+		int i = 0;
+		for(i = 12;data[i]!= 0x00;){
+			int count = (int)data[i];
+			//System.out.println(count); 
+			
+			for(int j = i+1; j<i +count + 1; j++){
+				name = name + (char)data[j];
+			}
+			i = i + count + 1;
+			if(data[i]!=0x00){
+				name = name + '.';
+			}
+		}
+		this.domainName = name;
+		i++;
+		int temp = (int)data[i] + (int)data[i+1];
+		this.queryType = temp;
+		
+		i = i+2;
+		temp = (int)data[i] + (int)data[i+1];
+		
+		this.queryStop = i + 1; // record the no.byte at which the query part stops
+		this.queryClass = temp;
+		
+		//System.out.println(queryType + "The type of the query");
+		//System.out.println(queryClass +"The class of the query");
+	}
+	
+}
